5446bce38994c7e4dd21b6ac996be94f7bb8c6d4
Gasper Lotric <lotric.gasper@gmail.com>
2021-03-30 02:19:09 +0200

    class vector

--- tracer.py
+++ tracer.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 
 f = open("podatki.txt", 'r')
@@ -11,3 +12,35 @@ camera = np.array([float(cam_pos[0]), float(cam_pos[1]), float(cam_pos[2])])
 ratio = float(width) / float(height)
 screen = (-1, 1 / ratio, 1, -1 / ratio) # L U R D
 
+class Vector:
+    
+    def __init__(self, x=0, y=0, z=0):
+        self.x = x
+        self.y = y
+        self.z = z
+
+class Vector(Vector):
+    def __repr__(self):
+        return 'Vector ({0}, {1}, {2})'.format(self.x, self.y, self.z)
+
+    def __str__(self):
+        return '({0}, {1}, {2})'.format(self.x, self.y, self.z)
+
+    def norm(self):
+        return np.sqrt(self.x**2 + self.y**2 + self.z**2)
+
+    def normalize(self):
+        norm = self.norm()
+        normalized = (self.x/norm, self.y/norm, self.z/norm)
+        return Vector(*normalized)
+
+    def dotproduct(self, other):
+        return self.x*other.x + self.y*other.y + self.z*other.z
+
+    def __add__(self, other):
+        added = (self.x+other.x, self.y+other.y, self.z+other.z)
+        return Vector(*added)
+
+    def __sub__(self, other):
+        subbed = (self.x-other.x, self.y-other.y, self.z-other.z)
+        return Vector(*subbed)
